01cc691390840c685558e760c8f0f727df9e6c85
Xavi Torelló <info@xaviertorello.cat>
2018-07-05 17:30:58 +0200

    Fix label 3.2

--- 3.2/run-qgis-3.2-in-docker.sh
+++ 3.2/run-qgis-3.2-in-docker.sh
@@ -1,6 +1,6 @@
 xhost +
 # --rm will remove the container as soon as it ends
-docker run --rm --name="qgis-desktop-3.0" \
+docker run --rm --name="qgis-desktop-3.2" \
 	-i -t \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
